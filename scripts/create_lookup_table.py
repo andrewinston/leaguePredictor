21e3d1b52f2e709d40d67f4f1aadfea4c33343ed
andre <a.winston.s97@gmail.com>
2019-10-28 15:10:08 -0300

    change lookup table calculation

--- scripts/create_lookup_table.py
+++ scripts/create_lookup_table.py
@@ -2,19 +2,25 @@ import sqlite3
 
 table = {}
 
-f = open("data/samples/attempt2/winrate_samples")
+f = open("data/samples/attempt2/winrate_samples.txt")
 matches = [[int(x) for x in a.split(' ')] for a in f.read().split("\n")]
-
+f.close()
 for match in matches:
 	for c1 in match[2:7]:
-		for c2 in match[7:]
+		for c2 in match[7:]:
 			if (c1, c2) not in table.keys():
 				table[(c1, c2)] = [1, 1] if match[0] == 1 else [0, 1]
-				table[(c2, c1)] = [0, 1] if match[0] == 1 else [1, 0]
+				table[(c2, c1)] = [0, 1] if match[0] == 1 else [1, 1]
 			else:
 				table[(c1, c2)][1] += 1
 				table[(c2, c1)][1] += 1
 				table[(c1, c2)][0] += 1 if match[0] == 1 else 0
 				table[(c2, c1)][0] += 0 if match[0] == 1 else 1
+
+output_path = "data/winrate_lookup_table.txt"
+f = open(output_path, "w+")
+
+for (k, v) in table.items():
+	f.write(str(k[0]) + " " + str(k[1]) + " " + str(float(v[0])/v[1]) + "\n")
 
 print(table)
